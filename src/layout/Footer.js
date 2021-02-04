b05b89bf5c4a72ed5999058b0dfbe55df06280ed
Pierre Moulin <phv.moulin@gmail.com>
2021-02-04 15:50:57 +0100

    add footer

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,8 +1,18 @@
 import './footer.css'
-
+import facebookLogo from '../assets/icons/facebook-logo.svg'
+import twitchLogo from '../assets/icons/twitch-logo.svg'
 
 export default function Footer() {
   return (
-    <footer>Hello from footer</footer>
+    <footer>
+      <ul className="social-media-list">
+        <li>
+          <img src={facebookLogo} alt="facebook" />
+        </li>
+        <li>
+          <img src={twitchLogo} alt="twitch" />
+        </li>
+      </ul>
+    </footer>
   )
 }
