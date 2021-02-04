c8a09753aa96c4a33e0bfdce8baa19f8eab8358d
Pierre Moulin <phv.moulin@gmail.com>
2021-02-04 16:48:02 +0100

    add form

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,3 +1,36 @@
-export default function ContactPage(){
-  return <>Hello from contacts</>
+import Section from '../components/Section'
+
+import './contactPage.css'
+
+export default function ContactPage() {
+
+  const handleSubmit = e => {
+    e.preventDefault()
+  }
+
+  return (
+    <>
+      <Section>
+        <h3>Contact</h3>
+        <p>
+          Pour toute information, ou pour passer commande, merci de laisser un message et d'inscrire
+          votre email. Je vous recontacte dès que possible.
+        </p>
+        <form onSubmit={handleSubmit} method="post" id="contactForm">
+          <label htmlFor="userEmail">Votre email</label>
+          <input id="userEmail" type="text"></input>
+          <label htmlFor="object">Objet</label>
+          <select id="object" name="object" form="contactForm">
+            <option value="Informations">Informations</option>
+            <option value="Forfait simple">Forfait simple</option>
+            <option value="Forfait cup">Forfait cup</option>
+            <option value="Forfait championnat">Forfait championnat</option>
+          </select>
+          <label htmlFor="userMessage">Message</label>
+          <textarea id="userMessage"></textarea>
+          <input className="submitButton" type="submit" />
+        </form>
+      </Section>
+    </>
+  )
 }
